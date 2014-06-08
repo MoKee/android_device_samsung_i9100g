e31b857a090f615e2912a024ada05c948037af7c
Ryuinferno <ryuinferno.xda@gmail.com>
2014-06-08 08:08:30 +0200

    i9100g: Temp hack for fixing camera
    Change-Id: If8adc15407d645af2cc576ca573c9d3576c7b7d5

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -134,3 +134,4 @@ TARGET_RELEASETOOL_IMG_FROM_TARGET_SCRIPT := ./device/samsung/i9100g/releasetool
 # Use the non-open-source parts, if they're present
 -include vendor/samsung/i9100g/BoardConfigVendor.mk
 
+TARGET_RELEASETOOLS_EXTENSIONS := device/samsung/i9100g/releasetools
